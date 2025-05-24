e7f1731e5364f91c948c6ecd6e31c6b5575ebfbe
Kelkjgmwl <joji.raprap@gmail.com>
2025-05-24 13:50:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,32 +4,33 @@ export default function BBQConstructor() {
       <h1 className="text-xl font-bold mb-4">Тестовая линия модулей</h1>
 
       <div className="overflow-x-auto bg-gray-100 p-4 rounded border">
-        <div className="flex items-end gap-0">
+        <div className="flex flex-row items-end space-x-0">
 
           {/* Первый модуль */}
-          <div className="relative w-[160px] h-[170px] flex-shrink-0">
+          <div className="relative w-40 h-44 flex-shrink-0">
             <img
               src="modules/mangal_700.png"
-              className="w-full h-[140px] object-contain"
+              className="w-full h-36 object-contain"
               alt="Мангал 700"
             />
             <div className="text-center text-sm mt-1">Мангал 700</div>
           </div>
 
           {/* Труба */}
-          <div className="w-[30px] bg-gray-400 text-white flex items-center justify-center text-xs h-[140px]">
+          <div className="w-8 bg-gray-400 text-white flex items-center justify-center text-xs h-36 flex-shrink-0">
             Труба
           </div>
 
-          {/* Второй модуль (перекрывает трубу) */}
-          <div className="relative -ml-[30px] w-[160px] h-[170px] flex-shrink-0 z-10">
+          {/* Второй модуль */}
+          <div className="relative -ml-8 w-40 h-44 flex-shrink-0 z-10">
             <img
               src="modules/koktal_800.png"
-              className="w-full h-[140px] object-contain"
+              className="w-full h-36 object-contain"
               alt="Коктал 800"
             />
             <div className="text-center text-sm mt-1">Коктал 800</div>
           </div>
+
         </div>
       </div>
     </div>
